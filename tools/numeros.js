82a8511c5e97c898d8952c3fafd1213d21e9e3fc
rjhalmeman <rjhalmeman@gmail.com>
2024-05-10 06:56:56 -0300

    10/05/2024 - 06:56:56

---
+++ tools/numeros.js
@@ -0,0 +1,12 @@
+function isNumber(value) {
+    return typeof value === 'number' && !isNaN(value);
+    /*
+     console.log(isNumber(42));      // Retorna true
+     console.log(isNumber("42"));    // Retorna false
+     console.log(isNumber("Hello")); // Retorna false
+     console.log(isNumber(NaN));     // Retorna false
+
+     para usar deve-se incluir o import 
+     <script src="../tools/numeros.js"></script> 
+    */
+}
