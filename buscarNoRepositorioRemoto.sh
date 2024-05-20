54c8c5cec2599177e184267596cd03bb57396830
rjhalmeman <rjhalmeman@gmail.com>
2024-05-20 15:59:16 -0300

    20/05/2024 - 15:59:16

---
+++ buscarNoRepositorioRemoto.sh
@@ -0,0 +1,3 @@
+# Busca no repositório remoto e atualiza o repositório local
+#por exemplo, se eu mandei coisas para o notebook e modifiquei. QAo voltar para o desktop, executo e ele deixa os diretórios iguais.
+git pull https://github.com/rjhalmeman/js.git
