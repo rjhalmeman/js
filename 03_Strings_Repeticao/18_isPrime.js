b6764cd3ceaaa3a7017d6d846feab53d70c663eb
rjhalmeman <rjhalmeman@gmail.com>
2024-06-10 15:29:49 -0300

    10/06/2024 - 15:29:49

--- 03_Strings_Repeticao/18_isPrime.js
+++ 03_Strings_Repeticao/18_isPrime.js
@@ -6,11 +6,24 @@ function isPrime(num) {
         return true;
     }
     let i = 2;
-    while (i < num) {
+    while (i < num) { //parte do pressuposto que se encontrou alguma divisão exata no intervalo, não é primo.
         if (num % i === 0) {
             return false;
         }
         i++;
+    }
+    return true;  
+}
+function ehPrimo(num) {
+    let cont = 0;
+    for (let i = 1; i <= num; i++) {      
+        if (num % i === 0) {
+            cont++;
+            // alert(i);
+            if (cont > 2) {
+                return false;
+            }
+        }
     }
     return true;
 }
--- 03_Strings_Repeticao/funcMedia.js
+++
@@ -1,8 +0,0 @@
-function calcularMedia(notas) {
-    let soma = 0
-    for (let i = 0; i < notas.length; i++) {
-        soma = soma + notas[i];
-    }
-    let media = soma / notas.length;
-    return media;
-}
