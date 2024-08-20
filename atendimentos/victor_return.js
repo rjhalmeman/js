9c87283f2900cd5b5cc2520b8e99b61ddc0b9ce7
radames <rjhalmeman@gmail.com>
2024-08-20 15:32:15 -0300

    atendimentos

---
+++ atendimentos/victor_return.js
@@ -0,0 +1,13 @@
+function somarDoisNumeros(a,b){
+    let s = a + b;
+    return s;
+}
+
+function maldete(){
+    for (let x = -10; x < 11; x++) {
+        y = 2*x**2+3*x-2;
+        console.log(x+" , "+y);        
+    }
+}
+
+maldete();
