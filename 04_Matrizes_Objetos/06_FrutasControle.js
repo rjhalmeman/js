074e2c95f06a6fe3f22d3004f91b533adaed5267
Radames <rjhalmeman@gmail.com>
2024-09-06 09:43:24 -0300

    06/09/2024 - 09:43:24

---
+++ 04_Matrizes_Objetos/06_FrutasControle.js
@@ -0,0 +1,37 @@
+let listaFrutas = [];
+
+function botaoInserir() {
+    //alert("deu certo o clic no botão");
+
+    const idFruta =
+        document.getElementById("inputId").value;
+    const nomeFruta =
+        document.getElementById("inputNome").value;
+    const corFruta =
+        document.getElementById("inputCor").value;
+    const pesoFruta =
+        document.getElementById("inputPeso").value;
+    let fruta =
+        new Fruta(idFruta, nomeFruta, corFruta, pesoFruta);
+    //alert(idFruta);
+    listaFrutas.push(fruta)
+}
+
+
+function botaoListar() {
+let saida = 
+   document.getElementById("outputListaDeFrutas");
+ saida.innerHTML = "";
+ for (let i = 0; i < listaFrutas.length; i++) {
+    let f = listaFrutas[i];
+    saida.innerHTML+= 
+        f.id+"-"+
+        f.nome+"-"+ 
+        f.cor+"-"+
+        f.peso+
+        "<br>";
+    
+ }
+
+
+}
