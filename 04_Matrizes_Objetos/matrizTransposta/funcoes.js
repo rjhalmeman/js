5bd10d6ceca163187f8f6f1c6d70242716e61def
Radames <rjhalmeman@gmail.com>
2024-09-19 15:07:54 -0300

    19/09/2024 - 15:07:54

---
+++ 04_Matrizes_Objetos/matrizTransposta/funcoes.js
@@ -0,0 +1,51 @@
+function converterDadosEmMatriz(dadosDeEntrada) {
+    // Separa em linhas
+    let linhas = dadosDeEntrada.trim().split("\n"); //o sinal para o split é no "\n", divide usando como sinal a quebra de linha
+    let matriz = [];
+
+    // Itera sobre cada linha
+    for (let i = 0; i < linhas.length; i++) {
+        // Separa os números por vírgula e converte-os para números
+        let numeros = linhas[i].split(",").map(Number);
+        matriz.push(numeros); //acrescenta os números na linha da variavel matriz
+    }
+    return matriz;
+}
+
+
+function gerarTransposta(matriz) {
+    let matrizTransporta = [];
+    for (let i = 0; i < matriz[0].length; i++) {
+        matrizTransporta[i] = [];
+        for (let j = 0; j < matriz.length; j++) {
+            matrizTransporta[i][j] = matriz[j][i];
+        }
+    }
+    return matrizTransporta
+}
+
+function matrizFormatadaParaHTML(matriz) {
+    // Converte a matriz para html com formatação para exibição
+    let output = "";
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            output += matriz[i][j].toString().padStart(3, ' '); // Adiciona o elemento atual à string de saída
+            if (j < matriz[i].length - 1) {
+                output += " "; //
+            }
+        }
+        output += "<br>"; // 
+    }
+    return output;
+}
+
+
+if (true) {
+
+    let matriz = [[2, 13, 4], [51, 6, 7]];
+    console.log(matrizFormatadaParaHTML(matriz));
+
+    let m = gerarTransposta(matriz);
+    console.log("------")
+    console.log(matrizFormatadaParaHTML(m));
+}
